5eeaba7baadff586f513793684fc9fc297f29c57
yeezy_na_izi <howone1sky@gmail.com>
2021-08-08 21:31:57 +0300

    Тупой css

--- back/education/urls.py
+++ back/education/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('1', views.test, name='test')
 ]
--- back/education/views.py
+++ back/education/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'education/home.html')
+
+
+def test(request):
+    context = {}
+    return render(request, 'education/home2.html')
