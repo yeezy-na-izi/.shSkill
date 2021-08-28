e09b26a4084a2be033a7c917bbdc4b0c357989d1
yeezy_na_izi <howone1sky@gmail.com>
2021-08-29 02:37:49 +0300

    add task page

--- back/education/urls.py
+++ back/education/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('courses/', views.courses_list, name='courses'),
     path('courses/<course_id>/', views.unique_course, name='unique_course'),
-    path('courses/<course_id>/<slug>/', views.tasks, name='unique_course'),
-    path('courses/<course_id>/<slug>/material/', views.materials, name='material')
+    path('courses/<course_id>/<slug>/', views.lesson_page, name='unique_course'),
+    path('courses/<course_id>/<slug>/material/', views.materials, name='material'),
+    path('courses/<course_id>/<slug>/<index>/', views.task, name='task')
 ]
